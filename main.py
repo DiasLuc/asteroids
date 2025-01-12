d0a681488b2c2a4d49e1d592c6b9cfc955c93401
Lucas Dias <diaslucp@gmail.com>
2025-01-12 14:20:35 -0500

    got player drawn on screen

--- main.py
+++ main.py
@@ -8,18 +8,31 @@ import pygame
 # from the database.py into the current file
 # from database import connect_database, database_version
 from constants import *
+from player import *
 
 def main():
     pygame.init()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    #  middle of screen
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        
+        player.draw(screen)
+        
+        # refreshes the screen; CALL THIS LAST AFTER DOING OTHER DRAW EVENTS!
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
+        
 if __name__ == "__main__":
     main()
