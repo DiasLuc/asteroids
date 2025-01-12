c6aa308f30e3509c2d0608b5ce7eca56bcf9fe32
Lucas Dias <diaslucp@gmail.com>
2025-01-12 12:42:07 -0500

    set up venv, got basic gui set up

--- main.py
+++ main.py
@@ -3,10 +3,23 @@
 # throughout this file
 import pygame
 
+# import the connect_database function
+# and the database_version variable
+# from the database.py into the current file
+# from database import connect_database, database_version
+from constants import *
+
 def main():
+    pygame.init()
     print("Starting asteroids!")
-
-
-
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
 if __name__ == "__main__":
     main()
